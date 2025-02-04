940e7596b49aa0a3e4b0fe0ebb53fdc54aa8c093
Weliton Guedes <weliton.guedes@outlook.com>
2025-02-03 21:36:39 -0300

    H2 database, test profile, JPA

--- src/main/java/com/guedes/aula/resources/UserResource.java
+++ src/main/java/com/guedes/aula/resources/UserResource.java
@@ -15,7 +15,7 @@ public class UserResource {
 	@GetMapping
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L, "Maria", "maria@gmail.com", "9999-9999", "12345");
-		return ResponseEntity.ok().body(u); 
+		return ResponseEntity.ok().body(u);
 	} 
 	
 	
