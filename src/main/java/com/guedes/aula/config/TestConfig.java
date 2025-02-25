9402c25c195663d200eb2afe3f25e7a218776d2d
Weliton Guedes <weliton.guedes@outlook.com>
2025-02-24 21:05:15 -0300

    Payment, one-to-one association

--- src/main/java/com/guedes/aula/config/TestConfig.java
+++ src/main/java/com/guedes/aula/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.guedes.aula.entities.Category;
 import com.guedes.aula.entities.Order;
 import com.guedes.aula.entities.OrderItem;
+import com.guedes.aula.entities.Payment;
 import com.guedes.aula.entities.Product;
 import com.guedes.aula.entities.User;
 import com.guedes.aula.entities.enums.OrderStatus;
@@ -83,7 +84,10 @@ public class TestConfig implements CommandLineRunner {
 			
 			orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 			
+			Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),  o1);
+			o1.setPayment(pay1);
 			
+			orderRepository.save(o1);
 		}
 		
 		
