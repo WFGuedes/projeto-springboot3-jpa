6e3d02c4cb396fac947aea1e1e94e79521186303
Weliton Guedes <weliton.guedes@outlook.com>
2025-03-24 21:20:41 -0300

    Exception handling - update

--- src/main/java/com/guedes/aula/services/UserService.java
+++ src/main/java/com/guedes/aula/services/UserService.java
@@ -13,6 +13,8 @@ import com.guedes.aula.repositories.UserRepository;
 import com.guedes.aula.services.exceptions.DatabaseException;
 import com.guedes.aula.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,9 +47,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	private void updateData(User entity, User obj) {
